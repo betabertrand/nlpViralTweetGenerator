3519f2c60a73d0122a08017e47024703228f6622
thenightowl00 <btsp60@yahoo.com>
2017-12-18 03:08:53 -0500

    creates getTopTrends and authenticate method

--- src/ViralTweet.java
+++ src/ViralTweet.java
@@ -6,7 +6,9 @@ import twitter4j.auth.RequestToken;
 import twitter4j.TwitterFactory;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ViralTweet {
@@ -14,35 +16,7 @@ public class ViralTweet {
     public static void main(String args[]) throws Exception{
         // The factory instance is re-useable and thread safe.
         Twitter twitter = TwitterFactory.getSingleton();
-        twitter.setOAuthConsumer("jGtXpSBZtcPsXQwUbcihEJaJ9", "qcEfFTwIXBOHJNKHwspmsRNfIOpSddR6od0lHGkBUcdmv37iqF");
-        RequestToken requestToken = twitter.getOAuthRequestToken();
-        AccessToken accessToken = null;
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        while (null == accessToken) {
-            System.out.println("Open the following URL and grant access to your account:");
-            System.out.println(requestToken.getAuthorizationURL());
-            System.out.print("Enter the PIN(if aviailable) or just hit enter.[PIN]:");
-            String pin = br.readLine();
-            try{
-                if(pin.length() > 0){
-                    accessToken = twitter.getOAuthAccessToken(requestToken, pin);
-                }else{
-                    accessToken = twitter.getOAuthAccessToken();
-                }
-            } catch (TwitterException te) {
-                if(401 == te.getStatusCode()){
-                    System.out.println("Unable to get the access token.");
-                }else{
-                    te.printStackTrace();
-                }
-            }
-        }
-        Trends popTrend;
-        popTrend = twitter.trends().getPlaceTrends(	2459115);
-
-        for (Trend t : popTrend.getTrends()) {
-            System.out.println(t.getName());
-        }
+        authenticate(twitter);
 
         System.out.println();
 
@@ -69,14 +43,47 @@ public class ViralTweet {
 
 
 
-        //persist to the accessToken for future reference.
-        storeAccessToken((int) twitter.verifyCredentials().getId() , accessToken);
-        //Status status = twitter.updateStatus(args[0]);
-        //System.out.println("Successfully updated the status to [" + status.getText() + "].");
-        //System.exit(0);
+
     }
     private static void storeAccessToken(int useId, AccessToken accessToken){
         //store accessToken.getToken()
         //store accessToken.getTokenSecret()
+    }
+
+    public static String[] getTopTrends(Twitter twitter) throws TwitterException{
+        Trends popTrend;
+        ArrayList<String> trendList = new ArrayList<>();
+        popTrend = twitter.trends().getPlaceTrends(	2459115 /* NYC */);
+        for (Trend t : popTrend.getTrends()) {
+            trendList.add(t.getName());
+        }
+
+        return (String[]) trendList.toArray();
+    }
+
+    public static void authenticate(Twitter twitter) throws TwitterException, IOException {
+        twitter.setOAuthConsumer("jGtXpSBZtcPsXQwUbcihEJaJ9", "qcEfFTwIXBOHJNKHwspmsRNfIOpSddR6od0lHGkBUcdmv37iqF");
+        RequestToken requestToken = twitter.getOAuthRequestToken();
+        AccessToken accessToken = null;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (null == accessToken) {
+            System.out.println("Open the following URL and grant access to your account:");
+            System.out.println(requestToken.getAuthorizationURL());
+            System.out.print("Enter the PIN(if aviailable) or just hit enter.[PIN]:");
+            String pin = br.readLine();
+            try{
+                if(pin.length() > 0){
+                    accessToken = twitter.getOAuthAccessToken(requestToken, pin);
+                }else{
+                    accessToken = twitter.getOAuthAccessToken();
+                }
+            } catch (TwitterException te) {
+                if(401 == te.getStatusCode()){
+                    System.out.println("Unable to get the access token.");
+                }else{
+                    te.printStackTrace();
+                }
+            }
+        }
     }
 }
